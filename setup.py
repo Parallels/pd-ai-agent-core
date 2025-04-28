98d473edf084a8f5f1b8946a8013ed16cc711c70
GitHub Action <action@github.com>
2025-04-28 00:14:13 +0000

    Bump version to 0.1.15

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name="pd-ai-agent-core",
-    version="0.1.14",
+    version="0.1.15",
     author="Parallels",
     author_email="carlos.lapao@parallels.com",
     description="A core library for AI agent functionality",
